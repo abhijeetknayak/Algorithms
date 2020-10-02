f9bf446558e6c6c73028ca412fedbfbef24e93d7
Abhijeet Nayak <abhijeetknayak@gmail.com>
2020-10-02 19:57:41 +0530

    DFS + graph reversal

--- Miscellaneous/SCC.h
+++ Miscellaneous/SCC.h
@@ -26,13 +26,27 @@ void Graph::add_edge(int start, int end) {
 void Graph::DFS(int source, bool explored[]) {
 	/* Recursive implementation */
 	explored[source] = true;
+	list<int>::iterator i;
+	for (i = adj[source].begin(); i != adj[source].end(); i++) {
+		if (explored[*i] == false) {
+			DFS(*i, explored);
+		}
+	}
+}
 
+Graph Graph::reverse_graph(void) {
+	Graph output(N);
+	for (int n = 0; n < N; n++) {
+		list<int>::iterator i;
+		for (i = adj[n].begin(); i != adj[n].end(); i++) {
+			output.adj[*i].push_back(n);
+		}
+	}
 
+	return output;
 }
 
 int main() {
-	string s;
-	cin >> s;
 
 	return 0;
 }
