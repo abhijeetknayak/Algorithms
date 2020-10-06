4c06106d28980d9187d913325e998d75e6252ac6
Abhijeet Nayak <abhijeetknayak@gmail.com>
2020-10-06 16:03:47 +0530

    Transitive closure matrix

---
+++ Miscellaneous/transitive-closure.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include "transitive-closure.h"
+
+using namespace std;
+
+int main() {
+    Graph g(7);
+    g.add_edge(0, 1);
+    g.add_edge(0, 2);
+    g.add_edge(1, 3);
+    g.add_edge(4, 1);
+    g.add_edge(6, 4);
+    g.add_edge(5, 6);
+    g.add_edge(5, 2);
+    g.add_edge(6, 0);
+
+    g.find_transitive_matrix();
+
+	return 0;
+}
--- Miscellaneous/transitive-closure.h
+++ Miscellaneous/transitive-closure.h
@@ -5,17 +5,24 @@ using namespace std;
 class Graph {
 	int N;  // Number of vertices
 	list<int> * adj;  // Adjacency list representation
+	bool ** tc;  // [V * V] matrix to store transitive closure matrix
 
 	public:
 	    Graph(int vertices);  // Constructor
 	    void add_edge(int start, int end);  // Directed edge
-	    void DFS(int source, bool explored[]);
+	    void DFS(int source, int end);
 	    void find_transitive_matrix();
 };
 
 Graph::Graph(int vertices) {
 	N = vertices;
 	adj = new list<int>[N];
+	tc = new bool * [N];
+	for (int i = 0; i < N; i++) {
+		tc[i] = new bool[N];
+		memset(tc[i], false, sizeof(bool) * N);
+	}
+
 }
 
 void Graph::add_edge(int start, int end) {
@@ -23,14 +30,25 @@ void Graph::add_edge(int start, int end) {
 	adj[start].push_back(end);
 }
 
-void Graph::DFS(int source, bool explored[]) {
+void Graph::DFS(int source, int end) {
 	/* Recursive implementation */
-	explored[source] = true;
+	tc[source][end] = true;
 	list<int>::iterator i;
 	for (i = adj[source].begin(); i != adj[source].end(); i++) {
-		if (explored[*i] == false) {
-			DFS(*i, explored);
+		if (!tc[source][*i]) {
+			DFS(source, *i);
 		}
+	}
+}
+
+void Graph::find_transitive_matrix(void) {
+	for (int i = 0; i < N; i++)
+		DFS(i, i);
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cout << tc[i][j] << " ";
+		}
+		cout << endl;
 	}
 }
 
