50d6382ec3422357ae92ccb6b1b9aa42dbb4b452
Abhijeet Nayak <abhijeetknayak@gmail.com>
2020-10-16 21:51:11 +0530

    Inserting node in a binary tree

--- Miscellaneous/Trees/binary-tree.h
+++ Miscellaneous/Trees/binary-tree.h
@@ -12,4 +12,35 @@ struct node {
 		left_child = NULL;
 		right_child = NULL;
 	}
+
+	node * insert_node(node * root_node, int data) {
+		if (root_node == NULL) {
+			root_node = new node(data);
+			return root_node;
+		}
+
+		// Traverse entire tree to find a NULL child element
+		queue<node*> nodes;
+		nodes.push(root_node);
+
+		while(!nodes.empty()) {
+			/* Do this until the queue is empty */
+			node * n = nodes.front();
+			nodes.pop();
+
+			if (n->left_child == NULL) {
+				node * new_node = new node(data);
+				n->left_child = new_node;
+				return root_node;
+			}
+			else { nodes.push(n->left_child); }
+
+			if (n->right_child == NULL) {
+				node * new_node = new node(data);
+				n->right_child = new_node;
+				return root_node;
+			}
+			else { nodes.push(n->right_child); }
+		}
+	}
 };
