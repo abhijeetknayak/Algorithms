f6033f8764faf856bdeab5573119ba2982e15ace
Abhijeet Nayak <abhijeetknayak@gmail.com>
2020-10-30 19:21:50 +0530

    Leetcode : Shortest palindrome adding chars from the back

--- Miscellaneous/LeetCode/shortest-palindrome.cpp
+++ Miscellaneous/LeetCode/shortest-palindrome.cpp
@@ -16,9 +16,24 @@ string shortestPalindromeNaive(string s) {
     return rev + s;
 }
 
+string shortestPalindromeNaive_reverse(string s) {
+    string rev = s;
+    reverse(rev.begin(), rev.end());
+    int n = s.length();
+    if (n == 0) return "";
+    for (int i = 0; i < n; i++) {
+        if (rev.substr(0, n - i) == s.substr(i)) {
+            return s.substr(0, i) + rev;
+        }
+    }
+    return s + rev;
+}
+
 int main() {
-	string s1 = "aababbabba";
+	string s1 = "aababbabbac";
 	cout << shortestPalindromeNaive(s1) << endl;
+
+	cout << shortestPalindromeNaive_reverse(s1) << endl;
 
 	return 0;
 }
