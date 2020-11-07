7079563d59b0d95575d3292181de6a2e00e897e6
Abhijeet Nayak <abhijeetknayak@gmail.com>
2020-11-07 22:54:59 +0530

    Deleting Nth node in linked list

---
+++ Miscellaneous/LeetCode/Linked-list-DeleteNthNode.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode{
+	int val;
+	ListNode * next;
+
+	ListNode(int value) {
+		val = value;
+		next = NULL;
+	}
+};
+
+ListNode* removeNthFromEnd(ListNode* head, int n) {
+    ListNode * current = head, * first, * second;
+    while (current != NULL) {
+
+        first->
+    }
+}
