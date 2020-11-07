b54575bd063e6a4105c14ba90520dcf6797fa5c6
Abhijeet Nayak <abhijeetknayak@gmail.com>
2020-11-07 22:55:46 +0530

    Leetcode : ZigZag conversion

---
+++ Miscellaneous/LeetCode/ZigZag-Conversion.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+string convert(string s, int numRows) {
+	vector<vector<char>> rows(numRows);
+	bool up = false;
+	int n = s.length();
+
+	for (int i = 0; i < n; i++) {
+
+	}
+}
