908c4bf8dc5e7c84734d436b443db4fe6cd3c6db
abhijeetknayak <abhijeetknayak@gmail.com>
2021-04-01 16:53:14 +0530

    Update MaxProfitKTransactions.cpp

--- Miscellaneous/AlgoExpert/MaxProfitKTransactions.cpp
+++ Miscellaneous/AlgoExpert/MaxProfitKTransactions.cpp
@@ -31,7 +31,29 @@ int maxProfitWithKTransactionsNoDP(vector<int> prices, int k) {
 	return dp[k][n - 1];
 }
 
+int maxProfitWithKTransactionsDP(vector<int> prices, int k) {
+    int n = prices.size();
+    int dp[k + 1][n], m_b = 0, m_b1 = 0;
+
+    for(int i = 0; i <= k; i++) {
+	for (int j = 0; j < n; j++) {
+	    dp[i][j] = 0;
+	}
+    }
+	
+    for (int i = 1; i <= k; i++) {
+	m_b = -INT_MAX;
+	for(int j = 1; j < n; j++) {
+	    m_b1 = max(m_b1, dp[i - 1][j - 1] - prices[j - 1]);
+	    dp[i][j] = max(dp[i][j - 1], m_b1 + prices[j]);
+	}
+    }
+    
+    return dp[k][n - 1];
+}
+
 int main() {
 	vector<int> prices {5, 11, 3, 50, 60, 90};
-	cout << maxProfitWithKTransactions(prices, 2);
+	cout << maxProfitWithKTransactionsNoDP(prices, 2);
+	cout << maxProfitWithKTransactionsDP(prices, 2);
 }
