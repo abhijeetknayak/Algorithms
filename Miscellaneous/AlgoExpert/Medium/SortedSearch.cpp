7f2ee1a9d8d47d647e55754e9778149e76edd4db
abhijeetknayak <abhijeetknayak@gmail.com>
2021-04-29 10:51:56 +0200

    Update SortedSearch.cpp

--- Miscellaneous/AlgoExpert/Medium/SortedSearch.cpp
+++ Miscellaneous/AlgoExpert/Medium/SortedSearch.cpp
@@ -2,28 +2,11 @@
 using namespace std;
 
 vector<int> searchInSortedMatrix(vector<vector<int>> matrix, int target) {
-	int r1 = 0, c1 = 0, r2 = matrix.size() - 1, c2 = matrix[0].size() - 1;
-	
-	while(r1 < r2 && c1 < c2) {
-		int m1 = r1 + (r2 - r1) / 2;
-		int m2 = c1 + (c2 - c1) / 2;
-		
-		if (matrix[m1][m2] == target) return vector<int>{m1, m2};
-		else if (matrix[m1][m2] > target) {
-			r2 = m1; c2 = m2;
-		}
-		else {
-			r1 = m1 + 1;
-			c1 = m2 + 1;
-		}
+	int row = 0, col = matrix[0].size() - 1;
+	while(row >= 0 && col >= 0) {
+		if (matrix[row][col] == target) return vector<int> {row, col};
+		else if (matrix[row][col] > target) col--;
+		else row++;
 	}
-	
-	for (int i = 0; i <= c1; i++) {
-		if (matrix[r1][i] == target) return vector<int> {r1, i};
-	}
-	
-	for(int i = 0; i <= r1; i++) {
-		if (matrix[i][c1] == target) return vector<int> {i, c1};
-	} 
   return vector<int> {-1, -1};
 }
