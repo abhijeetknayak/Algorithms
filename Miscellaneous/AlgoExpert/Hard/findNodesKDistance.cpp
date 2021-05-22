ae4c077ff62ed73d93c93b33203f5bcfc2a0daa1
abhijeetknayak <abhijeetknayak@gmail.com>
2021-05-22 16:29:56 +0200

    Update findNodesKDistance.cpp

--- Miscellaneous/AlgoExpert/Hard/findNodesKDistance.cpp
+++ Miscellaneous/AlgoExpert/Hard/findNodesKDistance.cpp
@@ -27,17 +27,44 @@ void DFS(BinaryTree* tree, BinaryTree* parent_node, unordered_map<int, BinaryTre
 	if (tree->right != NULL) DFS(tree->right, tree, parents);
 }
 
+vector<int> BFS_from_target(BinaryTree* start_node, unordered_map<int, BinaryTree*> parents, int k) {
+	vector<int> result, explored;
+	queue<pair<BinaryTree*, int>> frontier;
+	frontier.push(make_pair(start_node, 0));
+	
+	while(!frontier.empty()) {
+		pair<BinaryTree*, int> p = frontier.front(); frontier.pop();
+		if (find(explored.begin(), explored.end(), p.first->value) != explored.end()) {
+			continue;
+		}
+		int val = p.second;
+		if (val == k) { 
+			result.push_back(p.first->value); 
+			continue;
+		}
+		if (p.first->left != NULL) frontier.push(make_pair(p.first->left, val + 1));
+		if (p.first->right != NULL) frontier.push(make_pair(p.first->right, val + 1));
+		if (parents[p.first->value] != NULL) frontier.push(make_pair(parents[p.first->value], val + 1));
+		
+		explored.push_back(p.first->value);
+	}
+	return result;
+}
+
 vector<int> findNodesDistanceK(BinaryTree *tree, int target, int k) {
 	unordered_map<int, BinaryTree*> parents;
 	DFS(tree, NULL, parents);
+		
+	print_parents(parents);
+	cout << "Done!";
 	
 	BinaryTree* p = parents[target], * start_node;
-	if (p->left->value == target) start_node = p->left;
+	if (p == NULL) start_node = tree;
+	else if (p->left == NULL) start_node = p->right;
+	else if (p->right == NULL) start_node = p->left;
+	else if (p->left->value == target) start_node = p->left;
 	else start_node = p->right;
 	
-	BFS_from_target(start_node)
-	
-	print_parents(parents);
-	
-  return {};
+	vector<int> result = BFS_from_target(start_node, parents, k);	
+  return result;
 }
