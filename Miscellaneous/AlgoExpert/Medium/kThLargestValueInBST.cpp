2ba08399300b33f342b1130144aa8a4aec72db12
abhijeetknayak <abhijeetknayak@gmail.com>
2021-05-28 10:23:15 +0200

    Update kThLargestValueInBST.cpp

--- Miscellaneous/AlgoExpert/Medium/kThLargestValueInBST.cpp
+++ Miscellaneous/AlgoExpert/Medium/kThLargestValueInBST.cpp
@@ -9,7 +9,7 @@ public:
   BST(int value) { this->value = value; }
 };
 
-void DFS(BST* tree, vector<int>& result) {
+void DFS_naive(BST* tree, vector<int>& result) {
 	if (tree == NULL) return;
 	
 	DFS(tree->left, result);
@@ -17,10 +17,26 @@ void DFS(BST* tree, vector<int>& result) {
 	DFS(tree->right, result);
 }
 
-int findKthLargestValueInBst(BST *tree, int k) {
+int findKthLargestValueInBst_naive(BST *tree, int k) {
 	vector<int> result;
 	DFS(tree, result);
 	int index = result.size() - k;
 	if (index >= 0) return result[index];
   return -1;
+}
+
+void DFS(BST* tree, int k, int& num_elements_passed, int& result) {
+	if (tree == NULL || num_elements_passed == k) return;	
+
+	DFS(tree->right, k, num_elements_passed, result);
+	num_elements_passed += 1; 
+	if (num_elements_passed == k) result = tree->value;
+	DFS(tree->left, k, num_elements_passed, result);	
+}
+
+int findKthLargestValueInBst(BST *tree, int k) {
+	int result;
+	int num_passed = 0;
+	DFS(tree, k, num_passed, result);
+  return result;
 }
