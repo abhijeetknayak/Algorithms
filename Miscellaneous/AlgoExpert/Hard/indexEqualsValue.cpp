0d4c2505f188fb9f0865e8377882e7f1668c09f0
abhijeetknayak <abhijeetknayak@gmail.com>
2021-06-20 22:04:37 +0200

    Update indexEqualsValue.cpp

--- Miscellaneous/AlgoExpert/Hard/indexEqualsValue.cpp
+++ Miscellaneous/AlgoExpert/Hard/indexEqualsValue.cpp
@@ -1,9 +1,30 @@
 #include <vector>
 using namespace std;
 
-int indexEqualsValue(vector<int> array) {
+int indexEqualsValueBF(vector<int> array) {
 	for (int i = 0; i < array.size(); i++) {
 		 if (i == array[i]) return i;
 	}
   return -1;
+}
+
+int indexEqualsValue(vector<int> array) {
+	// Sorted Array
+	int first = 0, last = array.size() - 1, ret = -1, mid = 0;
+	while(first <= last) {
+		mid = first + (last - first) / 2;
+		if (array[mid] == mid) {
+			last = mid - 1;
+			ret = mid;
+		}
+		else if (array[mid] < mid) {
+			// Go to the right side
+			first = mid + 1;
+		}
+		else {
+			// Go left
+			last = mid - 1;
+		}
+	}
+  return ret;
 }
