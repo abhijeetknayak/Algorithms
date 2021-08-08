bb1598c35b723b3bbdcbf39325259d696778ac90
abhijeetknayak <abhijeetknayak@gmail.com>
2021-08-08 07:44:56 +0200

    Create DP-robHouses2.cpp

---
+++ Miscellaneous/LeetCode/DP-robHouses2.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.size() == 0) return 0;
+        if (nums.size() == 1) return nums[0];
+        if (nums.size() == 2) return max(nums[0], nums[1]);
+        
+        vector<int> dp(nums.size() - 1, 0);
+        dp[0] = nums[0];
+        dp[1] = nums[1];
+        int max_val = max(dp[0], dp[1]);
+        
+        for (int i = 2; i < dp.size(); i++) {
+            dp[i] = max(dp[i - 1] - nums[i - 1] + nums[i],
+                       dp[i - 2] + nums[i]);
+            if (dp[i] > max_val) max_val = dp[i];
+        }
+        
+        dp[0] = nums[1];
+        dp[1] = nums[2];
+        max_val = max(max_val, nums[2]);
+        
+        for (int i = 2; i < dp.size(); i++) {
+            dp[i] = max(dp[i - 1] - nums[i] + nums[i + 1],
+                       dp[i - 2] + nums[i + 1]);
+            if (dp[i] > max_val) max_val = dp[i];
+        }
+        
+        return max_val;
+    }
+};
