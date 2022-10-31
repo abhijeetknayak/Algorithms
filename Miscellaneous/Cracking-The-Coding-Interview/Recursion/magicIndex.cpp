7f6e2f68c7c16a6883ccb3176486007029666c58
Abhijeet Nayak <abhijeetknayak@gmail.com>
2022-10-31 22:53:31 +0100

    Efficient code for robot path computation

---
+++ Miscellaneous/Cracking-The-Coding-Interview/Recursion/magicIndex.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool find_magic_index(vector<int> list, int start_idx, int end_idx)
+{
+
+}
+
+int main()
+{
+    vector<int> list {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+
+
+
+
+
+
+    for (auto ele : list)
+    {
+        cout << ele << " ";
+    }
+
+
+
+
+    return 0;
+}
--- Miscellaneous/Cracking-The-Coding-Interview/Recursion/robotPath.cpp
+++ Miscellaneous/Cracking-The-Coding-Interview/Recursion/robotPath.cpp
@@ -1,9 +1,55 @@
 #include <iostream>
+#include <map>
 #include <vector>
 
 using namespace std;
 
-bool findPath(int row, int col, vector<vector<bool>> map, vector<pair<int, int>>& path)
+bool findPathEfficient(int row, int col, vector<vector<bool>> grids, vector<pair<int, int>>& path, 
+                       map<pair<int, int>, bool>& dp)
+{
+    bool success = false;
+    if (row < 0 || col < 0) return false;
+    pair<int, int> point = make_pair(row, col);
+    
+    if (dp.find(point) != dp.end())
+    {
+        return dp.at(point);
+    }
+
+    if (row == 0 && col == 0) 
+    {
+        // Path to source found, return
+        // path.push_back(make_pair(0, 0));
+        return true;
+    }
+
+    // Can only go left or up from the destination
+
+    //Left
+    if (col - 1 >= 0 && grids[row][col - 1] != 0) success = findPathEfficient(row, col - 1, grids, path, dp);
+    if (success) 
+    {
+        // Found path, add (row, col - 1) to path
+        path.push_back(make_pair(row, col - 1));
+        dp[make_pair(row, col - 1)] = true;
+        return success;
+    }
+
+    if (row - 1 >= 0 && grids[row - 1][col] != 0) success = findPathEfficient(row - 1, col, grids, path, dp);
+    if (success) 
+    {
+        // Found path, add (row, col - 1) to path
+        path.push_back(make_pair(row - 1, col));
+        dp[make_pair(row - 1, col)] = true;
+        return success;
+    }    
+
+    // If no path found yet, return false
+    dp[make_pair(row, col)] = false;
+    return false;    
+}
+
+bool findPathNaive(int row, int col, vector<vector<bool>> grids, vector<pair<int, int>>& path)
 {
     bool success = false;
     if (row < 0 || col < 0) return false;
@@ -17,7 +63,7 @@ bool findPath(int row, int col, vector<vector<bool>> map, vector<pair<int, int>>
     // Can only go left or up from the destination
 
     //Left
-    if (col - 1 >= 0 && map[row][col - 1] != 0) success = findPath(row, col - 1, map, path);
+    if (col - 1 >= 0 && grids[row][col - 1] != 0) success = findPathNaive(row, col - 1, grids, path);
     if (success) 
     {
         // Found path, add (row, col - 1) to path
@@ -25,7 +71,7 @@ bool findPath(int row, int col, vector<vector<bool>> map, vector<pair<int, int>>
         return success;
     }
 
-    if (row - 1 >= 0 && map[row - 1][col] != 0) success = findPath(row - 1, col, map, path);
+    if (row - 1 >= 0 && grids[row - 1][col] != 0) success = findPathNaive(row - 1, col, grids, path);
     if (success) 
     {
         // Found path, add (row, col - 1) to path
@@ -41,7 +87,7 @@ int main()
 {
     int rows = 5, cols = 5;
 
-    vector<vector<bool>> map = { {1, 1, 0, 0, 1},
+    vector<vector<bool>> grids = { {1, 1, 0, 0, 1},
                                  {0, 1, 1, 1, 0},
                                  {1, 0, 0, 1, 1},
                                  {1, 1, 1, 0, 1},
@@ -49,8 +95,16 @@ int main()
 
     vector<pair<int, int>> path;
 
-    bool success = findPath(rows - 1, cols - 1, map, path);
+    bool success = findPathNaive(rows - 1, cols - 1, grids, path);
     path.push_back(make_pair(rows - 1, cols - 1));
+    cout << success << endl;
+    for (auto ele : path)
+    {
+        cout << ele.first << " " << ele.second << endl;
+    }
+
+    map<pair<int, int>, bool> dp;
+    success = findPathEfficient(rows, cols, grids, path, dp);
     cout << success << endl;
     for (auto ele : path)
     {
